64b5c28f3358ca347e4f19f72ab110f51ccea94b
afrum <83051384+afrum@users.noreply.github.com>
2021-06-18 13:09:35 +1200

    multiple videos defined times and videos in csv
    using a csv file with video name, start time, endtime
    to create new output videos

---
+++ multitrim.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+# to trim multiple videos from a csv file called times.csv
+# the format in the csv file is video_name,start_time,end_time
+
+
+i=0
+
+while IFS="," read f1 f2 f3
+do
+	
+	i=$((i+1))
+	ffmpeg -i $f1 -ss $f2 -to $f3 outtime_$i.MOV </dev/null
+	
+done < times.csv
+
+
+# see the link below for why I use </dev/null at the end
+# https://unix.stackexchange.com/questions/36310/strange-errors-when-using-ffmpeg-in-a-loop
+

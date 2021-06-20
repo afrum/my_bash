a83a699c40961d28438ebc474aafd05965d1623c
afrum <83051384+afrum@users.noreply.github.com>
2021-06-20 20:01:20 +1200

    trim and crop multiple videos
    trims and crops multiple videos when video_name,start_time,end_time,crop_details data are in the first four columns of a csv file

---
+++ multitrimcrop.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+# to trim multiple videos from a csv file called times.csv
+# the format in the csv file is video_name,start_time,end_time
+
+
+i=0
+
+while IFS="," read f1 f2 f3 f4 
+do
+	
+	
+	ffmpeg -i $f1 -ss $f2 -to $f3 -vf crop=$f4 outtimecrop_$i.MOV </dev/null
+	i=$((i+1))
+
+done < timecrop.csv
+
+
+# see the link below for why I use </dev/null at the end
+# https://unix.stackexchange.com/questions/36310/strange-errors-when-using-ffmpeg-in-a-loop

9e295f6283ff0da6059a87811ae8f25fe29b1452
afrum <83051384+afrum@users.noreply.github.com>
2022-02-21 13:43:17 +1300

    Update speedvid.sh

--- speedvid.sh
+++ speedvid.sh
@@ -2,9 +2,12 @@
 
 inputvideo=input.mkv
 
-# the number for the video PTS is the reciprocal for the audio and viceversa.
+# to sync video and audio the video PTS is the reciprocal for the audio and viceversa.
 # keep between .5 and 2 in order to get the best results.
 # to speed up or slow down more you can run command multiple times.
+# 2 for video speed slows the video by half .5 speeds the video up to double speed.
+# 2 for audio speed speeds the audio up to double speed .5 slows it to half speed.
+
 
 speedvideo=2
 speedaudio=1/$speedvideo
